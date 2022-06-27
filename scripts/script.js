ba989f4584dc72fbe5a56a840e720397ab45f241
Danylo Prudnikov <danylo.prudnikov@gmail.com>
2022-06-27 15:34:58 +0300

    interact at JS in the prices section

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,27 @@
-$(document).ready(function(){
-	$('.slider').slick({
-		arrows:true,
-		dots:false,
-		slidesToShow:2,
-		autoplay:true,
-		speed:1000,
-		autoplaySpeed:1500,
-		responsive:[
+$(document).ready(function () {
+	$(".slider").slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 2,
+		autoplay: true,
+		speed: 1000,
+		autoplaySpeed: 1500,
+		responsive: [
 			{
 				breakpoint: 768,
 				settings: {
-					slidesToShow:1
-				}
-			}
-		]
+					slidesToShow: 1,
+				},
+			},
+		],
+	});
+});
+
+const box = document.querySelectorAll(".box");
+
+box.forEach((i) => {
+	i.addEventListener("click", function () {
+		box.forEach((el) => el.classList.remove("active"));
+		this.classList.add("active");
 	});
 });
