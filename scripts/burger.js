fb7d23bb663f48c24f8b40c62aede9fffa124d69
Danylo Prudnikov <danylo.prudnikov@gmail.com>
2022-07-04 22:02:55 +0300

    burger

---
+++ scripts/burger.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const button = document.getElementById('btn-menu')
+    const menu = document.getElementById('list-menu')
+    button.classList.toggle('active')
+    menu.classList.toggle('active')
+}	
